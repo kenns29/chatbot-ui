01163b33bd276ab90ad9f2b19ed363455f0f8b81
Hong Wang <kennsmacintosh@gmail.com>
2023-05-05 17:31:32 -0700

    ui changes (#4)

--- components/Chat/SystemPrompt.tsx
+++ components/Chat/SystemPrompt.tsx
@@ -194,7 +194,7 @@ export const SystemPrompt: FC<Props> = ({
   return (
     <div className="flex flex-col">
       <label className="mb-2 text-left text-neutral-700 dark:text-neutral-400">
-        {t('System Prompt')}
+        {t('Add Documents')}
       </label>
       <textarea
         ref={textareaRef}
